ed7936321629e5519c39c266971c9be0ce217d8d
elitniy <elitniy.07@gmail.com>
2018-10-10 14:04:20 +0400

    Translator

---
+++ Bing Translator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TranslatedText = new System.Windows.Forms.TextBox();
+            this.TraslateText = new System.Windows.Forms.TextBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // TranslatedText
+            // 
+            this.TranslatedText.Location = new System.Drawing.Point(6, 213);
+            this.TranslatedText.Multiline = true;
+            this.TranslatedText.Name = "TranslatedText";
+            this.TranslatedText.Size = new System.Drawing.Size(369, 92);
+            this.TranslatedText.TabIndex = 7;
+            // 
+            // TraslateText
+            // 
+            this.TraslateText.Location = new System.Drawing.Point(6, 95);
+            this.TraslateText.Multiline = true;
+            this.TraslateText.Name = "TraslateText";
+            this.TraslateText.Size = new System.Drawing.Size(369, 92);
+            this.TraslateText.TabIndex = 6;
+            this.TraslateText.TextChanged += new System.EventHandler(this.TextChanged_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Cyril-Latin",
+            "Latin-Cyril"});
+            this.comboBox.Location = new System.Drawing.Point(109, 49);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(160, 21);
+            this.comboBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 371);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.TranslatedText);
+            this.Controls.Add(this.TraslateText);
+            this.Name = "Form1";
+            this.Text = "Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.TextBox TranslatedText;
+        internal System.Windows.Forms.TextBox TraslateText;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
---
+++ Bing Translator/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> words = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            words.Add("а", "a");
+            words.Add("б", "b");
+            words.Add("в", "v");
+            words.Add("г", "g");
+            words.Add("д", "d");
+            words.Add("е", "e");
+            words.Add("ё", "yo");
+            words.Add("ж", "j");
+            words.Add("з", "z");
+            words.Add("и", "i");
+            words.Add("й", "y");
+            words.Add("к", "k");
+            words.Add("л", "l");
+            words.Add("м", "m");
+            words.Add("н", "n");
+            words.Add("о", "o");
+            words.Add("п", "p");
+            words.Add("р", "r");
+            words.Add("с", "s");
+            words.Add("т", "t");
+            words.Add("у", "u");
+            words.Add("ф", "f");
+            words.Add("х", "h");
+            words.Add("ц", "ts");
+            words.Add("ч", "ç");
+            words.Add("ш", "ş");
+            words.Add("щ", "ş");
+            words.Add("ъ", "'");
+            words.Add("ы", "i");
+            words.Add("ь", "'");
+            words.Add("э", "e");
+            words.Add("ю", "yu");
+            words.Add("я", "ya");
+            words.Add("А", "A");
+            words.Add("Б", "B");
+            words.Add("В", "V");
+            words.Add("Г", "G");
+            words.Add("Д", "D");
+            words.Add("Е", "E");
+            words.Add("Ё", "Yo");
+            words.Add("Ж", "J");
+            words.Add("З", "Z");
+            words.Add("И", "I");
+            words.Add("Й", "Y");
+            words.Add("К", "K");
+            words.Add("Л", "L");
+            words.Add("М", "M");
+            words.Add("Н", "N");
+            words.Add("О", "O");
+            words.Add("П", "P");
+            words.Add("Р", "R");
+            words.Add("С", "S");
+            words.Add("Т", "T");
+            words.Add("У", "U");
+            words.Add("Ф", "F");
+            words.Add("Х", "H");
+            words.Add("Ц", "Ts");
+            words.Add("Ч", "Ç");
+            words.Add("Ш", "Ş");
+            words.Add("Щ", "Ş");
+            words.Add("Ъ", "'");
+            words.Add("Ы", "I");
+            words.Add("Ь", "'");
+            words.Add("Э", "E");
+            words.Add("Ю", "Yu");
+            words.Add("Я", "Ya");
+        }
+        private void TextChanged_Click(object sender, EventArgs e)
+        {
+            string source = TraslateText.Text;
+            if (comboBox.Text == "Choose")
+            {
+                MessageBox.Show("Make a choice for translation");
+            }
+            if (comboBox.Text == "Cyril-Latin")
+            {
+                foreach (KeyValuePair<string, string> pair in words)
+                {
+                    if (comboBox.Text == "Cyril-Latin") {
+}
+                        source = source.Replace(pair.Key, pair.Value);
+                }
+            }
+            if (comboBox.Text == "Latin-Cyril")
+            {
+                foreach (KeyValuePair<string, string> pair in words)
+                {
+                    source = source.Replace(pair.Value, pair.Key);
+                }
+            }
+            TranslatedText.Text = source;
+        }
+    }
+}
